308901d4469a1975013e211791fca14964f806d8
Bot Maker <chooseusername9@protonmail.com>
2020-08-23 16:45:44 -0500

    - Main now handles the input args for nesting level and parsing a csv file. Initial implementation for threadingn now added to the WebCrawler.

--- src/CrawlerMain.scala
+++ src/CrawlerMain.scala
@@ -3,10 +3,41 @@ import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 
-object CrawlerMain {
-   def main(args: Array[String]): Unit = {
-      val crawler = new WebCrawler("https://scrapethissite.com/pages/")
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
-      crawler.getPageLinks(crawler.url)
+object CrawlerMain {
+   /*
+   ** args(0): csv file of website URLs to crawl
+   ** args(1): Nesting level
+   */
+   def main(args: Array[String]): Unit = {
+      var urlList: ListBuffer[String] = new ListBuffer[String]
+      var nestingLevel: Int = 0
+
+      // If a CSV has been provided, populate a list with the values
+      if (args.length > 0) {
+         urlList = ListBuffer.from(args(0).split(",").toList)
+
+         // If a nesting level has been provided, store it
+         if (args.length > 1) {
+            nestingLevel = args(1).toInt
+         }
+      }
+      else {
+         // Note: The website limits requests to a maximum of 1 per second.
+         urlList.addOne("https://scrapethissite.com/")
+      }
+
+      // Create a crawler for each URL provided
+      var crawlerList: ListBuffer[WebCrawler] = new mutable.ListBuffer
+      for(url <- urlList) {
+         crawlerList.addOne(new WebCrawler(url))
+         crawlerList(0).run()
+      }
+
+
+
+//      crawler.getPageLinks(crawler.url)
    }
 }
--- src/WebCrawler.scala
+++ src/WebCrawler.scala
@@ -4,14 +4,25 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.{Document, Element}
 import org.jsoup.select.Elements
 
+import scala.collection.mutable
 import scala.collection.mutable.HashSet
 
-class WebCrawler(var url: String) {
-   var crawlUrl: String = url
-   val urlLinks: HashSet[String] = HashSet.empty[String]
+class WebCrawler(var url: String) extends Thread {
+   var rootUrl: String = url
+   var urlLinks: HashSet[String] = new mutable.HashSet[String]()
 
+   def getCrawlerUrl(): String = {
+      return this.rootUrl
+   }
    def setCrawlUrl(url : String): Unit = {
-      this.crawlUrl = url
+      this.rootUrl = url
+   }
+
+   def getRootUrl(): String = {
+      return this.rootUrl
+   }
+   def setRootUrl(newRoot: String): Unit = {
+      this.rootUrl = newRoot
    }
 
    def getPageLinks(url: String): Unit = {
@@ -19,10 +30,19 @@ class WebCrawler(var url: String) {
       var linksOnPage: Elements = htmlPage.select("a[href]")
       var iterator: util.Iterator[Element] = linksOnPage.iterator()
 
-      while (iterator.hasNext) {
-//         getPageLinks(iterator.next().attr("abs:href"))
-         printf(iterator.next().toString)
-      }
+      // If the url has not been visited then add it to the urlLinks container
+      if (!urlLinks.contains(url)) {
+         urlLinks.add(url)
 
+         while (iterator.hasNext) {
+            //         getPageLinks(iterator.next().attr("abs:href"))
+            println(iterator.next().toString)
+         }
+
+      }
+   }
+
+   override def run(): Unit = {
+      println("Doing thread stuff, please come back later.")
    }
 }
